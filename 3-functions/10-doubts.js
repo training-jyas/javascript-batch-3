461060d2b1c5edeadcaca054abe520229d5764c8
Jyasveer Gotta <jyasveer@gmail.com>
2017-12-09 12:03:59 +0530

    iife done

---
+++ 3-functions/10-doubts.js
@@ -0,0 +1,20 @@
+// iife vs closures
+// (function (){
+//     console.log('func 1');
+
+//     test();
+
+//     function test(){
+//         console.log('test');
+//     }
+// })();
+
+// writing something to console and html
+var obj = {
+    'name': 'RTST',
+    'location': 'btm'
+};
+
+document.writeln(obj);
+console.log(obj);
+console.log(user);
